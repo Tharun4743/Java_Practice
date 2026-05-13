1af0f9cfba45860ea2ca10f6ca27b8ee4b3d089e
Tharunkumar K <tharunkumark42007@gmail.com>
2026-05-13 14:31:11 +0530

    Create Swap.java

---
+++ Basics/Day 1/Swap.java
@@ -0,0 +1,19 @@
+class Swap
+{
+    public static void main (String args []){
+
+        int a =10;
+        int b = 20;
+        int c = 0;
+
+        //a = a+b;
+        //b=a-b;
+        //a = a-b;
+
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    } 
+}
